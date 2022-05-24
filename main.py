7656f8fc57b0d95a59a60622596a79be1b6f1bb7
Bliss Bizoza <100536801+BlissBiz@users.noreply.github.com>
2022-05-24 23:58:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import*
+import random
+
+
+
+
+
+    
+#************** Starting Point of Program *************#
+if __name__ == "__main__":
+  root = Tk()
+  root.title("Action Moive Quiz")
+  quizStarter_object = QuizStarter(root)
+  root.mainloop()
